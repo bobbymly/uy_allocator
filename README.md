bd1bd4e9ad6c7364bdb8ea297c86cb1c63e9030b
bobbymly <1402278356@qq.com>
2018-04-27 16:45:08 +0800

    latest

---
+++ README.md
@@ -0,0 +1,1 @@
+# uy_alloctor
---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "uy_alloctor_2.h"
+#include <string.h>
+using namespace std;
+
+
+int main()
+{
+    uy_alloctor_2<0> alloc;
+
+    //测试正常使用
+    char* test1 = (char*)alloc.allocate(64);
+    strcpy(test1,"apply 64 bytes, the alloctor will add 19 blocks of 64 bytes to the free_list");
+    cout<<test1<<endl;
+
+
+    //测试非正常使用，检验 free_list 是否如设计，程序崩溃则为正常
+    char* test2 = (char*)alloc.allocate(1);
+    strcpy(test2,"the blocks is continous in the memory, if we use the memory out of 19*64=1216 bytes,the program will crash");
+    cout<<test2<<endl;
+    for(int i=0;i<1500;++i)
+    {
+        test2[i]=i;
+        cout<<(int)test2[i];
+    }
+
+
+
+    return 0;
+}
